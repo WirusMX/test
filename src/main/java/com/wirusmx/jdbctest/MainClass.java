d8a5a5e98e0369882b0434d0bb45324531d37ae6
wirusmx <piunovm@mail.ru>
2016-09-29 09:44:03 +0300

    jdbc

--- src/main/java/com/wirusmx/jdbctest/MainClass.java
+++ src/main/java/com/wirusmx/jdbctest/MainClass.java
@@ -5,49 +5,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.*;
 
-public class MainClass
-{
-    public static void main( String[] args ) throws ClassNotFoundException, IOException {
+public class MainClass {
+    public static void main(String[] args) throws ClassNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
 
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Database name: ");
         String dbName = consoleReader.readLine();
 
-        Connection connection;
-        Statement statement;
-        try {
-            connection = DriverManager.getConnection("jdbc:sqlite:" + dbName);
-            statement = connection.createStatement();
-        } catch (SQLException e) {
-            System.out.println("ERROR: " + e.getMessage());
-            return;
-        }
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + dbName);
+             Statement statement = connection.createStatement()) {
 
-        String query;
-        System.out.print("> ");
-        while (!(query = consoleReader.readLine()).equalsIgnoreCase("exit")) {
-            try {
-                boolean execute = statement.execute(query);
-                if (!execute) {
-                    System.out.println("OK");
-                    System.out.print("> ");
-                    continue;
-                }
-
-                ResultSet resultSet = statement.getResultSet();
-                while (resultSet.next()){
-                    for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
-                        System.out.print(resultSet.getString(i) + "\t\t");
-                    }
-                    System.out.println();
-                }
-
-            } catch (SQLException e) {
-                System.out.println("ERROR: " + e.getMessage());
+            String query;
+            System.out.print("> ");
+            while (!(query = consoleReader.readLine()).equalsIgnoreCase("exit")) {
+                handleQuery(statement, query);
+                System.out.print("> ");
             }
 
-            System.out.print("> ");
+        } catch (SQLException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+
+
+    }
+
+    private static void handleQuery(Statement statement, String query) {
+        try {
+            boolean execute = statement.execute(query);
+            if (!execute) {
+                System.out.println("OK");
+                return;
+            }
+
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()) {
+                for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+                    System.out.print(resultSet.getString(i) + "\t\t");
+                }
+                System.out.println();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("ERROR: " + e.getMessage());
         }
     }
 }
